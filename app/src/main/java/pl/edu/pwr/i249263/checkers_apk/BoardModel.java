e27b1b3544fc95ad09c3433d5438e415691a30cb
kondiiq <kowalczykkon@gmail.com>
2021-04-24 21:55:34 +0200

    Upgrade BoardModel

--- app/src/main/java/pl/edu/pwr/i249263/checkers_apk/BoardModel.java
+++ app/src/main/java/pl/edu/pwr/i249263/checkers_apk/BoardModel.java
@@ -1,4 +1,37 @@
 package pl.edu.pwr.i249263.checkers_apk;
 
 public class BoardModel {
+
+//   Variables
+    private final int WidthArea = 10;
+    private final int HeightArea = 10;
+    private  final  int SizeSingleArea = 25;
+
+//    Construtor
+    public BoardModel()
+    {
+        int[][] Board;
+        Board = new int[getWidthArea()][getHeightArea()];
+    }
+
+//    Others Methods
+    public void CreateBoard()
+    {
+
+    }
+
+    public int getWidthArea()
+    {
+        return this.WidthArea;
+    }
+
+    public int getHeightArea()
+    {
+        return this.HeightArea;
+    }
+
+    public int getSizeSingleArea()
+    {
+        return this.SizeSingleArea;
+    }
 }
